0b046dcf0006055b02141667b682bd53ef49b28b
Nurkholis <noerkholis.madjid@gmail.com>
2025-06-09 15:17:20 +0700

    ✨ feat(clerk - supabase):  Implement Auth, Billing, Database Setup, Create Companion Server Action

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,13 @@
 import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 
@@ -19,11 +27,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${bricolage.variable} antialiased`}>
+      <html lang="en">
+        <body className={`${bricolage.variable} antialiased`}>
+        <ClerkProvider>
           <Navbar />
           {children}
-      </body>
-    </html>
+        </ClerkProvider>
+        </body>
+      </html>
   );
 }
